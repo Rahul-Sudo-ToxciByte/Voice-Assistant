a722c77a58c31ada851176ad722e10b3d0db9203
G. Rahul <119603975+Rahul-Sudo-ToxciByte@users.noreply.github.com>
2025-06-11 09:22:33 +0530

    Delete modules directory

--- modules/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Jarvis Assistant Modules Package
- 
-This package contains all the core modules for the Jarvis assistant.
-""" 
--- modules/gmail_control.py
+++
@@ -1,7 +0,0 @@
-class GmailControl:
-    def __init__(self, gmail_integration):
-        self.gmail = gmail_integration
-    def check_email(self, *args, **kwargs):
-        return "Gmail check email functionality is not implemented yet."
-    def send_email(self, *args, **kwargs):
-        return "Gmail send email functionality is not implemented yet." 
